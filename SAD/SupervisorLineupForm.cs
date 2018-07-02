aa1132bdd7be0b0fd0c693d930454083208a6abf
Jarrod <Jarrod@LAPTOP-7AD7LTHS>
2018-07-02 11:47:08 +0800

    cas

---
+++ SAD/SupervisorLineupForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class SupervisorLineupForm : Form
+    {
+        public SupervisorLineupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SupervisorLineupForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            StaffManagForm staffManagFormObject1 = new StaffManagForm();
+            staffManagFormObject1.Show();
+        }
+    }
+}
